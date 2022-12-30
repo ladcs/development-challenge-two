be40f7081d3d4803d131ca046eff952a8f5b02f9
ladcs <hpladcs@gmail.com>
2022-12-30 15:12:04 -0300

    fix: getAll frontend to backend

--- frontend/src/context/myContext.ts
+++ frontend/src/context/myContext.ts
@@ -5,8 +5,6 @@ import IPatient from '../interface/IPatient';
 type INITIAL = {
   data: IPatient[],
   setData: React.Dispatch<React.SetStateAction<IPatient[]>>,
-  numberPatients: number,
-  setNumberPatients: React.Dispatch<React.SetStateAction<number>>,
   filter: IFilter,
   setFilter: React.Dispatch<React.SetStateAction<IFilter>>,
 }
@@ -14,8 +12,6 @@ type INITIAL = {
 const INITIAL_STATE: INITIAL = {
   data: [] as IPatient[],
   setData: () => {},
-  numberPatients: 0,
-  setNumberPatients: () => {},
   filter: {
     filterByEmail: '',
     filterByName: '',
--- frontend/src/context/provider.tsx
+++ frontend/src/context/provider.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect , useState } from 'react';
 import MyContext from './myContext';
 import IPatient from '../interface/IPatient';
-// import axios from 'axios';
+import axios from 'axios';
 import IFilter from '../interface/IFilter';
-import simulaData from '../data';
 
 interface IProps {
   children: React.ReactNode;
@@ -14,12 +13,10 @@ interface IProps {
 const Provider = ({ children }:IProps) => {
   const [data, setData] = useState<IPatient[]>([]);
   const [getData, setGetData] = useState<boolean>(true)
-  const [numberPatients, setNumberPatients] = useState<number>(0)
   const [filter, setFilter] = useState<IFilter>({
     filterByEmail: '',
     filterByName: '',
   });
-/*
   const saveAPI = async () => {
     try {
       const {data} = await axios.get('https://fgfi62nwy1.execute-api.us-east-1.amazonaws.com/patient');
@@ -33,29 +30,11 @@ const Provider = ({ children }:IProps) => {
         }
       });
       setData(fixDate);
-      setNumberPatients(data.count);
       setGetData(false);
     } catch (error) {
       console.log(error);
     }
-  };*/
-
-  const saveAPI = async () => {
-    const fixDate = simulaData
-    .patients.map((patient) => {
-      const fix = patient.birthDate.split('/');
-      return {
-        birthDate: [fix[1], fix[0], fix[2]].join('/'),
-        patientName: patient.patientName,
-        email: patient.email,
-        address: patient.address,
-    }
-    });
-    setData(fixDate);
-    setNumberPatients(simulaData.count);
-    setGetData(false);
   };
-
   useEffect(() => {
     if (getData) saveAPI();
   });
@@ -65,8 +44,7 @@ const Provider = ({ children }:IProps) => {
     setFilter,
     data,
     setData,
-    numberPatients,
-    setNumberPatients };
+  };
   return (
     <MyContext.Provider value={ { state: contextValue } }>
       { children }
